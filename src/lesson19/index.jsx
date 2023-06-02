c98be12129697689ad96310c1e685945e0cde4f4
= <=>
2023-06-03 01:21:51 +0500

    Axios

---
+++ src/lesson19/index.jsx
@@ -0,0 +1,23 @@
+import React , { useEffect } from 'react'
+import axios from 'axios'
+
+const index = () => {
+    
+  return (
+    <>
+    {
+        useEffect(() => {
+            axios({
+                url:'https://jsonplaceholder.typicode.com/users',
+                method:'GET',
+                headers:{},
+                body:{name:'jasurbek',age:19},
+                // timeout:3000,
+            }).then(res => console.log(res)) 
+        } , [])
+    }
+    </>
+  )
+}
+
+export default index
